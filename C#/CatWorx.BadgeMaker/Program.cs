59a3dd4a6e5550b12d8f844f25d669c4645750e0
Khadijah <128011155+kali20987@users.noreply.github.com>
2023-08-03 12:19:02 -0400

    Add files via upload

---
+++ C#/CatWorx.BadgeMaker/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace CatWorx.BadgeMaker
+{
+  class Program
+  {
+    static List<Employee> GetEmployees()
+    {
+      List<Employee> employees = new List<Employee>();
+      while (true)
+      {
+        Console.WriteLine("Enter first name (leave empty to exit): ");
+        string firstName = Console.ReadLine();
+        if (firstName == "")
+        {
+          break;
+        }
+        Console.Write("Enter last name: ");
+        string lastName = Console.ReadLine();
+
+        Console.Write("Enter ID: ");
+        int id = Int32.Parse(Console.ReadLine());
+
+        Console.Write("Enter Photo URL:");
+        string photoUrl = Console.ReadLine();
+
+        Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+        employees.Add(currentEmployee);
+      }
+      return employees;
+    }
+    static void PrintEmployees(List<Employee> employees)
+    {
+      Util.PrintEmployees(employees);
+    }
+    static void Main(string[] args)
+    {
+      List<Employee> employees = GetEmployees();
+      PrintEmployees(employees);
+
+      Util.PrintEmployees(employees);
+      Util.MakeCSV(employees);
+    }
+  }
+}
